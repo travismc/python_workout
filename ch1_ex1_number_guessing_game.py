f63d29071a45ad555da7284cd6dac4580960c1a6
Travis M <travis.mchugh@gmail.com>
2024-10-06 20:12:52 -0700

    Added solution to Guessing Game exercise

---
+++ ch1_ex1_number_guessing_game.py
@@ -0,0 +1,33 @@
+# For this exercise
+
+# Write a function (guessing_game) that takes no arguments.
+# When run, the function chooses a random integer between 0 and 100 (inclusive).
+# Then ask the user to guess what number has been chosen.
+# Each time the user enters a guess, the program indicates one of the following:
+# Too high
+# Too low
+# Just right
+# If the user guesses correctly, the program exits. Otherwise, the user is asked to try again.
+# The program only exits after the user guesses correctly.
+# We’ll use the randint (http://mng.bz/mBEn) function in the random module to generate a random number.
+
+import random
+
+
+def guessing_game():
+    number = random.randint(0, 100)
+
+    while True:
+        guess = int(input('Guess a number between 0 and 100: '))
+
+        if guess == number:
+            print(f'{guess} is exactly right!')
+            break
+
+        if guess > number:
+            print(f'{guess} is too high')
+        else:
+            print(f'{guess} is too low')
+
+
+guessing_game()
