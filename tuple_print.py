465cddbdf366d8ed8ee8cc60c6141cc9672e1f13
Travis M <travis.mchugh@gmail.com>
2024-10-16 21:28:15 -0700

    Added comments on important concepts

--- tuple_print.py
+++ tuple_print.py
@@ -13,9 +13,9 @@ PEOPLE = [('Donald', 'Trump', 7.85),
 
 def format_sort_records(list_of_tuples):
     output = []
-    template = '{1:10} {0:10} {2:5.2f}'
+    template = '{1:10} {0:10} {2:5.2f}'  # string formatting for template
     for person in sorted(list_of_tuples,
-                         key=itemgetter(1, 0)):
+                         key=itemgetter(1, 0)):  # use of itemgetter as key
 
         output.append(template.format(*person))
     return output
