77a6450b7910a8b647c00951971c9b598e6b0899
Travis M <travis.mchugh@gmail.com>
2024-10-17 23:24:35 -0700

    Added starting code for Restaurant exercise

---
+++ restaurant.py
@@ -0,0 +1,11 @@
+# Create a new constant dict, called MENU, representing the possible items you can order at a restaurant. The keys will be strings, and the values will be prices (i.e., integers). You should then write a function, restaurant, that asks the user to enter an order:
+
+# If the user enters the name of a dish on the menu, the program prints the price and the running total. It then asks the user again for their order.
+# If the user enters the name of a dish not on the menu, the program scolds the user (mildly). It then asks the user again for their order.
+# If the user enters an empty string, the program stops prompting and prints the total amount.
+
+MENU = {}
+
+
+def restaurant():
+    pass
