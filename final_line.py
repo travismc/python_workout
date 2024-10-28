7cc82e78ec56b40e1d0ce6fb4f1df8f04656b792
Travis M <travis.mchugh@gmail.com>
2024-10-27 19:25:22 -0700

    Added solution for Final Line

--- final_line.py
+++ final_line.py
@@ -1,5 +1,11 @@
 # Write a function (get_final_line) that takes a filename as an argument. The function should return that file’s final line on the screen.
 
 
-def get_final_line(file):
-    pass
+def get_final_line(filename):
+    final_line = ''
+    for current_line in open(filename):
+        final_line = current_line
+    return final_line
+
+
+print(get_final_line('test.txt'))
