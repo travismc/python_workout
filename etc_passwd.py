63a1ce0dc3633f648745f13812f221269e7dd2a6
Travis M <travis.mchugh@gmail.com>
2024-10-30 20:15:13 -0700

    Added solution to etc/passwd to dict problem

--- etc_passwd.py
+++ etc_passwd.py
@@ -1,11 +1,18 @@
 # Write a function, passwd_to_dict, that reads from a Unix-style “password file,” commonly stored as /etc/passwd, and returns a dict based on it.
 
+# Each line is one user record, divided into colon-separated fields. The first field (index 0) is the username, and the third field (index 2) is the user’s unique ID number. (In the system from which I took the /etc/passwd file, nobody has ID -2, root has ID 0, and daemon has ID 1.) For our purposes, you can ignore all but these two fields.
 
-# sample data
-nobody: *: -2: -2: : 0: 0: Unprivileged User: / var/empty: / usr/bin/false
-root: *: 0: 0: : 0: 0: System Administrator: / var/root: / bin/sh
-daemon: *: 1: 1: : 0: 0: System Services: / var/root: / usr/bin/false
+# sample data:
+# fake_passwd_file
+
+def passwd_to_dict(filename):
+    users = {}
+    with open(filename) as passwd:
+        for line in passwd:
+            if not line.startswith(('#', '\n')):
+                user_info = line.split(':')
+                users[user_info[0]] = int(user_info[2])
+    return users
 
 
-def passwd_to_dict():
-    pass
+print(passwd_to_dict('fake_passwd_file'))
