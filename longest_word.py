24c4597e65aee4780d5aab1f4d906a4acf40b523
Travis M <travis.mchugh@gmail.com>
2024-11-03 17:10:14 -0800

    Added comments on important concept

---
+++ longest_word.py
@@ -0,0 +1,14 @@
+# Write two functions. find_longest_word takes a filename as an argument and returns the longest word found in the file. The second function, find_all_longest_words, takes a directory name and returns a dict in which the keys are filenames and the values are the longest words from each file.
+
+# Book files are at ~Developer/python_workout/books/*
+
+import os
+
+
+def find_longest_word(filename):
+    longest_word = ""
+    pass
+
+
+def find_all_longest_words():
+    pass
