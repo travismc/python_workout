626ff9fe609be437432464eaa503d1312e52e097
Travis M <travis.mchugh@gmail.com>
2024-11-05 19:41:51 -0800

    Added starting code for Reading and Writing CSV exercise

---
+++ csv_passwd.py
@@ -0,0 +1,7 @@
+# Create a function, passwd_to_csv, that takes two filenames as arguments: the first is a passwd-style file to read from, and the second is the name of a file in which to write the output.
+
+# The new file’s contents are the username (index 0) and the user ID (index 2). Note that a record may contain a comment, in which case it will not have anything at index 2; you should take that into consideration when writing the file. The output file should use TAB characters to separate the elements.
+
+
+def passwd_to_csv(input_file, output_file):
+    pass
