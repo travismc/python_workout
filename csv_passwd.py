d818583545c8a5a6e03d976727939610d29e8bd6
Travis M <travis.mchugh@gmail.com>
2024-11-06 20:09:16 -0800

    Added solution to Reading and Writing CSV

--- csv_passwd.py
+++ csv_passwd.py
@@ -1,7 +1,17 @@
 # Create a function, passwd_to_csv, that takes two filenames as arguments: the first is a passwd-style file to read from, and the second is the name of a file in which to write the output.
 
-# The new file’s contents are the username (index 0) and the user ID (index 2). Note that a record may contain a comment, in which case it will not have anything at index 2; you should take that into consideration when writing the file. The output file should use TAB characters to separate the elements.
+# The new file’s contents are the username (index 0) and the user ID (index 2). Note that a record may contain a comment, in which case it will not have anything at index 2; you should take that into consideration when writing the file. The output file should use TAB characters to separate the elements. You can assume that any line with at least two colon-separated fields is legitimate.
+
+import csv
 
 
-def passwd_to_csv(input_file, output_file):
-    pass
+def passwd_to_csv(passwd_filename, csv_filename):
+    with open(passwd_filename) as passwd, open(csv_filename, 'w') as output:
+        infile = csv.reader(passwd, delimiter=':')
+        outfile = csv.writer(output, delimiter='\t')
+        for record in infile:
+            if len(record) > 1:
+                outfile.writerow((record[0], record[2]))
+
+
+passwd_to_csv('test.csv', 'output.csv')
